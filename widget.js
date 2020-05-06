4e4efb400c779aeab88cc5a0c400157c5a13b70e
Sven Latham <sven@noggin.bi>
2020-05-06 06:29:02 +0100

    We can now select and show current date

--- widget.js
+++ widget.js
@@ -10,14 +10,36 @@ var highstreetWidget = (function() {
     var svgns = "http://www.w3.org/2000/svg";
 
     /* formatting options */
-    var gh = 100; // Total height of grid
+    var gh = 70; // Total height of grid
+    var gy = 30; // Top of the main chart (where MAX will be)
     
     var cols = []; // This will hold 12 columns/rectangles
+    var elDays = []; // Accumulate days here (0..6)
     function init() {
+    
         el = document.getElementById("highstreetwidget");
         el.style.width = "400px";
         el.style.height = "230px"; 
         el.style.backgroundColor = "#cccccc";
+
+        let dateContainer = document.createElement("DIV");
+        for (let i=0; i<7; i++) {
+            let div = document.createElement("DIV");
+            div.appendChild(document.createTextNode(days[i]));
+            div.style.width = "35px";
+            div.style.padding = "10px";
+            div.style.display = "inline-block";
+            div.style.textAlign = "center";
+            div.style.cursor = "pointer";
+            dateContainer.appendChild(div);
+            div.addEventListener("click", function() {
+                showDay(i+1);
+            });
+            elDays[i]= div;
+        }
+        el.appendChild(dateContainer);
+
+
 
         var svg = document.createElementNS(svgns, "svg");
         svg.setAttribute("viewBox", "0 0 400 200");
@@ -26,7 +48,7 @@ var highstreetWidget = (function() {
         for (let i=0; i<12; i++) {
             let rect = document.createElementNS(svgns, "rect");
             rect.setAttributeNS(null, "x", 20+(i*30));
-            rect.setAttributeNS(null, "y", 20);
+            rect.setAttributeNS(null, "y", gy);
             rect.setAttributeNS(null, "width", 25);
             rect.setAttributeNS(null, "height", gh);
             rect.setAttributeNS(null, "fill", "#336699");
@@ -35,43 +57,43 @@ var highstreetWidget = (function() {
         }
 
         // Position some text:
-        {
+        function renderTime(x, val) {
             var text = document.createElementNS(svgns, "text");
-            text.setAttributeNS(null, "x", 5);
-            text.setAttributeNS(null, "y", gh+25);
-            text.appendChild(document.createTextNode("7am"));
+            text.setAttributeNS(null, "x", x-13);
+            text.setAttributeNS(null, "y", gy+gh+25);
+            text.setAttributeNS(null, "font-size", "16px");
+            text.appendChild(document.createTextNode(val));
             svg.appendChild(text);
+            var line = document.createElementNS(svgns, "line");
+            line.setAttributeNS(null, "x1", x);
+            line.setAttributeNS(null, "x2", x);
+            line.setAttributeNS(null, "y1", gy+gh+5);
+            line.setAttributeNS(null, "y2", gy+gh+10);
+            line.setAttributeNS(null, "stroke", "gray");
+            svg.appendChild(line);
         }
-        {
-            var text = document.createElementNS(svgns, "text");
-            text.setAttributeNS(null, "x", 125);
-            text.setAttributeNS(null, "y", gh+25);
-            text.appendChild(document.createTextNode("11am"));
-            svg.appendChild(text);
-        }
-        {
-            var text = document.createElementNS(svgns, "text");
-            text.setAttributeNS(null, "x", 245);
-            text.setAttributeNS(null, "y", gh+25);
-            text.appendChild(document.createTextNode("3pm"));
-            svg.appendChild(text);
-        }
-        {
-            var text = document.createElementNS(svgns, "text");
-            text.setAttributeNS(null, "x", 365);
-            text.setAttributeNS(null, "y", gh+25);
-            text.appendChild(document.createTextNode("7pm"));
-            svg.appendChild(text);
-        }
-
+        renderTime(18, "7am");
+        renderTime(138, "11am");
+        renderTime(258, "3pm");
+        renderTime(378, "7pm");
+        
         // Draw the line
         var line = document.createElementNS(svgns, "line");
         line.setAttributeNS(null, "x1", 5);
         line.setAttributeNS(null, "x2", 390);
-        line.setAttributeNS(null, "y1", 5);
-        line.setAttributeNS(null, "y2", 5);
+        line.setAttributeNS(null, "y1", gy);
+        line.setAttributeNS(null, "y2", gy);
         line.setAttributeNS(null, "stroke", "red");
         svg.appendChild(line);
+        {
+            var text = document.createElementNS(svgns, "text");
+            text.setAttributeNS(null, "x", 5);
+            text.setAttributeNS(null, "y", gy-3);
+            text.setAttributeNS(null, "font-size", "10px");
+            text.setAttributeNS(null, "fill", "red");
+            text.appendChild(document.createTextNode("MAX"));
+            svg.appendChild(text);
+        }
         
         
             
@@ -88,20 +110,27 @@ var highstreetWidget = (function() {
         getData();
     }
 
-    function showDay(i) {
+    function showDay(index) {
+        let today = new Date();
+        let day = today.getDay();
+        if (day == 0) { day = 7; }
+        if (!index) { index = day; }
+
+
+        // Day is 1-indexed, so we need to subtract 1 in most cases
         // Set the rects for this day
-        var row = data[i];
+        var row = data[index-1];
         for(let i=0; i<12; i++) {
             
-            cols[i].setAttribute("y", gh - ((gh/max)*row[i]));
+            cols[i].setAttribute("y", gy + (gh - ((gh/max)*row[i])));
             cols[i].setAttribute("height", ((gh/max)*row[i]));
         }
 
-    }
+        for (let i=0; i<7; i++) {
+            elDays[i].style.fontWeight = (i==index-1 ? 'bold': 'normal');
+        }
 
-    function showHour(h) {
-        // Highlight the selected hour
-
+        // If it's today's date, highlight the hour in particular:
 
     }
 
@@ -116,7 +145,8 @@ var highstreetWidget = (function() {
         // data has 7 x 12 values (covering 0700-1900); max has the max value. Go do your work!
         console.log(data);
         console.log(max);
-        showDay(1);
+
+        showDay(); // Without an argument, it shows current day
     }
 
     function dataLoaded(input) {
